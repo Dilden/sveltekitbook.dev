a7ee89f769db4e15fe59f00336a199626149ca57
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-05-03 15:38:37 -0500

    Initial commit

---
+++ src/lib/Section.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let color;
+</script>
+
+<div class='w-full {color}'>
+  <div class='container mx-auto'>
+    <slot>
+      default section content
+      this should span the entire screen width
+      and should have nice background colors
+    </slot>
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Button from "$lib/tools/Button.svelte"
+  import Section from "$lib/Section.svelte"
+</script>
+
+<Section color="bg-amber-600">
+  <h1 class='text-6xl text-center text-emerald-400'>SvelteKit Up and Running</h1>
+  <Button />
+</Section>
+<Section color="bg-slate-300"/>
+<Section color="bg-sky-400"/>
+<Section color="bg-slate-300"/>
