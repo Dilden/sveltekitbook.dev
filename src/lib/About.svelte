886efbe16ccd52e961a951e7890f5d2ec618baf9
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-06-28 15:53:10 -0500

    More content added to About section

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,8 +1,7 @@
-<h2>About the Book</h2>
-<p>
-This book aims to be an easy-to-read guide introducing you to the core concepts of the refreshingly simple JavaScript (JS) framework known
-as SvelteKit. It was written to highlight what I felt were the core concepts of the framework. Because
-the JS landscape changes so frequently, this text should not be considered an authority on SvelteKit.
-Rather, the intention behind this book is to provide a resource that I wish was available to me when
-I was learning this promising new framework.
+<h2 class='text-2xl my-5'>About the Book</h2>
+<p class='py-3'>
+  This book aims to be an easy-to-read guide introducing you to the core concepts of the refreshingly simple yet powerful JavaScript (JS) framework known as SvelteKit. It was written to highlight what I felt were the core concepts of the framework.
+</p>
+<p class='py-3'>
+  Whether you’re new to JavaScript development and are wondering which framework to pick up first, or are a seasoned developer tired of the bloat that comes with other popular frameworks; this book will teach you the concepts behind the project leading the field in revolutionizing JavaScript development. The examples found throughout the book are concise, practical, and easy to follow along with while still effectively demonstrating how to leverage the various techniques necessary for implementing features commonly found in web applications.
 </p>
