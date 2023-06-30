44056851158cf79c115c866bb24f9a3c1eba6454
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2023-06-29 22:33:17 -0500

    Fixed bottom left rounded corner on last nav item

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -35,7 +35,7 @@
     <ul class='list-none flex lg:flex-row flex-col justify-evenly gap-x-1 gap-y-0.5 flex-nowrap '>
       {#each sections as section, x}
         <li class='w-full text-right lg:text-center'>
-          <a class='{x == 0 ? "lg:rounded-bl-lg" : ""} {x == 3 ? "rounded-b-lg lg:rounded-br-lg " : "" } block w-full p-2 lg:p-5 text-lg text-amber-600 transition-all ease-in-out duration-300 hover:bg-[#242323] lg:hover:scale-110' href='#{section.href}'>
+          <a class='{x == 0 ? "lg:rounded-bl-lg" : ""} {x == 3 ? "rounded-b-lg lg:rounded-bl-none lg:rounded-br-lg " : "" } block w-full p-2 lg:p-5 text-lg text-amber-600 transition-all ease-in-out duration-300 hover:bg-[#242323] lg:hover:scale-110' href='#{section.href}'>
            {section.title}
           </a>
         </li>
