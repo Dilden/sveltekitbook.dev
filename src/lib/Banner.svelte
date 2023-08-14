44382f875a8ce1f5a262daffeb23169554fee102
dylan <dylan.hildenbrand@gmail.com>
2023-08-14 12:30:59 -0500

    Adjusted buy now button to be before book image on mobile

--- src/lib/Banner.svelte
+++ src/lib/Banner.svelte
@@ -4,12 +4,12 @@
   import Book2 from '$lib/Book2.svelte'
 </script>
 
-<div class='min-h-full grid grid-rows-2 lg:grid-rows-1 grid-cols-6 justify-items-center gap-y-28 lg:gap-y-3 m-2 lg:m-5'>
-  <div class='flex flex-col row-start-1 col-span-6 col-start-1 lg:col-span-3 lg:col-start-1 justify-center'>
+<div class='min-h-full grid grid-rows-[auto-1fr] lg:grid-rows-1 grid-cols-6 justify-items-center gap-y-28 lg:gap-y-3 m-2 lg:m-5'>
+  <div class='flex flex-col row-start-2 lg:row-start-1 col-span-6 col-start-1 lg:col-span-3 lg:col-start-1 justify-center'>
     <Book />
     <Book2 />
   </div>
-  <div class='flex flex-col row-start-2 col-span-4 col-start-2 lg:col-span-3 lg:col-start-4 lg:row-start-1 gap-y-5 m-2 lg:m-5'>
+  <div class='flex flex-col row-start-1 col-span-4 col-start-2 lg:col-span-3 lg:col-start-4 lg:row-start-1 gap-y-5 m-2 lg:m-5'>
     <h1 class='text-6xl text-center text-slate-200'>SvelteKit Up and Running</h1>
     <h2 class='text-2xl text-center text-slate-200'>Leverage the power of a next-generation web framework to build high-performance web apps with ease</h2>
     <h3 class='text-xl text-center text-slate-200'>By Dylan Hildenbrand</h3>
