146c670afbc59ab2e0818a3c37317969d8e6c7b8
dylan <dylan.hildenbrand@gmail.com>
2025-01-22 15:35:11 -0600

    better alt text on img

--- src/lib/Author.svelte
+++ src/lib/Author.svelte
@@ -6,61 +6,102 @@
 	<div class="justify-self-start grid grid-cols-3 gap-4 order-2 lg:order-1 py-2">
 		<h2 class="text-2xl col-span-3">Dylan Hildenbrand</h2>
 		<p class="col-span-3">
-Dylan Hildenbrand is a freelance full-stack web developer with a penchant for understanding how things work. Over the past decade, that desire to know “how things work” has led Dylan to a robust skillset ranging from front and back-end development to database administration, Linux server administration, and automation of application deployments and testing procedures. Dylan graduated from Minot State University in 2012 with a degree in Computer Science and dove headfirst into the world of web development. While he enjoys working with JavaScript and Node.js, he also has years of training with PHP, WordPress, and Yii2. When not proselytizing to strangers about the benefits to free and open source software (FOSS), Dylan enjoys administering and automating his “homelab.” To read more of his ramblings about web development and why Vim is the best text editor, visit <a href="https://www.closingtags.com/" target="_blank">https://www.closingtags.com</a>.
-    </p>
-    <!-- GitHub -->
-    <span class='col-span-1 text-center grid justify-items-center'>
-      <a href='https://github.com/Dilden' target="_blank" class="svg_link transition ease-in-out duration-300">
-        <svg viewBox="0 0 20 20" 
-          class="w-[85px] trasition-all ease-in-out hover:translate-y-1 hover:scale-110 duration-300"
-          version="1.1" 
-          xmlns="http://www.w3.org/2000/svg" 
-          fill="#000000"
-          stroke="#000000">
-          <title>Dylan's GitHub Profile</title>
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-          <g id="SVGRepo_iconCarrier">
-              <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)" fill="#f97141" class='fill_me'>
-                    <g id="icons" transform="translate(56.000000, 160.000000)">
-                      <path d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399" id="github-[#142]"></path>
-                    </g>
-                </g>
-              </g>
-          </g>
-        </svg>
-      </a>
-    </span>
-    <!-- Mastodon -->
+			Dylan Hildenbrand is a freelance full-stack web developer with a penchant for understanding
+			how things work. Over the past decade, that desire to know “how things work” has led Dylan to
+			a robust skillset ranging from front and back-end development to database administration,
+			Linux server administration, and automation of application deployments and testing procedures.
+			Dylan graduated from Minot State University in 2012 with a degree in Computer Science and dove
+			headfirst into the world of web development. While he enjoys working with JavaScript and
+			Node.js, he also has years of training with PHP, WordPress, and Yii2. When not proselytizing
+			to strangers about the benefits to free and open source software (FOSS), Dylan enjoys
+			administering and automating his “homelab.” To read more of his ramblings about web
+			development and why Vim is the best text editor, visit <a
+				href="https://www.closingtags.com/"
+				class="transition-all"
+				target="_blank">https://www.closingtags.com</a
+			>.
+		</p>
+		<!-- GitHub -->
 		<span class="col-span-1 text-center grid justify-items-center">
-      <svg
-        class="w-[115px] -mt-[15px] transition ease-in-out hover:translate-y-1 hover:scale-110 duration-300" 
-        xmlns="http://www.w3.org/2000/svg"
-        aria-label="Mastodon"
-        role="img"
-        viewBox="0 0 512 512"
-        fill="#ffffff"
-        stroke="#ffffff"
-        transform="rotate(0)">
-        <title>Dylan's Mastodon Profile</title>
-        <g id="SVGRepo_bgCarrier" stroke-width="0" /><g
-          id="SVGRepo_tracerCarrier"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke="#CCCCCC"
-          stroke-width="3.072"
-        /><g id="SVGRepo_iconCarrier">
-          <a xlink:href="https://mastodon.social/@closingtags" target="_blank" class='transition-all ease-in-out duration-300 fill-[#f97141] hover:fill-[#8e3d20]'>
-            <path d="m409 290c-5 24-43 50-85 56-86 11-137-6-137-6 3 13-4 54 70 52 31 0 58-7 58-7l2 27c-51 24-107 15-140 6-67-17-79-90-81-162v-59c0-74 49-96 49-96 50-24 180-22 222 0 0 0 49 22 49 96 0 0 1 55-7 93" />
-            <path d="m358 202v91h-35v-88c0-18-8-27-23-27-18 0-27 11-27 33v47h-34v-47c0-22-9-33-27-33-15 0-23 9-23 27v88h-35v-91c0-18 5-60 52-60 39 0 50 37 50 37s10-37 50-37c45 0 52 42 52 60" fill='#cbd5e1' />
-          </a>
-        </g>
-      </svg>
+			<a
+				href="https://github.com/Dilden"
+				target="_blank"
+				class="svg_link transition ease-in-out duration-300"
+			>
+				<svg
+					viewBox="0 0 20 20"
+					class="w-[85px] trasition-all ease-in-out hover:translate-y-1 hover:scale-110 duration-300"
+					version="1.1"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="#000000"
+					stroke="#000000"
+				>
+					<title>Dylan's GitHub Profile</title>
+					<g id="SVGRepo_bgCarrier" stroke-width="0" />
+					<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+					<g id="SVGRepo_iconCarrier">
+						<g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+							<g
+								id="Dribbble-Light-Preview"
+								transform="translate(-140.000000, -7559.000000)"
+								fill="#f97141"
+								class="fill_me"
+							>
+								<g id="icons" transform="translate(56.000000, 160.000000)">
+									<path
+										d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399"
+										id="github-[#142]"
+									/>
+								</g>
+							</g>
+						</g>
+					</g>
+				</svg>
+			</a>
 		</span>
-    <!-- Website -->
+		<!-- Mastodon -->
 		<span class="col-span-1 text-center grid justify-items-center">
-        <a class="transition ease-in-out hover:translate-y-1 hover:scale-110 duration-300" href="https://www.closingtags.com/" target="_blank">
+			<svg
+				class="w-[115px] -mt-[15px] transition ease-in-out hover:translate-y-1 hover:scale-110 duration-300"
+				xmlns="http://www.w3.org/2000/svg"
+				aria-label="Mastodon"
+				role="img"
+				viewBox="0 0 512 512"
+				fill="#ffffff"
+				stroke="#ffffff"
+				transform="rotate(0)"
+			>
+				<title>Dylan's Mastodon Profile</title>
+				<g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+					id="SVGRepo_tracerCarrier"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke="#CCCCCC"
+					stroke-width="3.072"
+				/><g id="SVGRepo_iconCarrier">
+					<a
+						xlink:href="https://mastodon.social/@closingtags"
+						target="_blank"
+						class="transition-all ease-in-out duration-300 fill-[#f97141] hover:fill-[#8e3d20]"
+					>
+						<path
+							d="m409 290c-5 24-43 50-85 56-86 11-137-6-137-6 3 13-4 54 70 52 31 0 58-7 58-7l2 27c-51 24-107 15-140 6-67-17-79-90-81-162v-59c0-74 49-96 49-96 50-24 180-22 222 0 0 0 49 22 49 96 0 0 1 55-7 93"
+						/>
+						<path
+							d="m358 202v91h-35v-88c0-18-8-27-23-27-18 0-27 11-27 33v47h-34v-47c0-22-9-33-27-33-15 0-23 9-23 27v88h-35v-91c0-18 5-60 52-60 39 0 50 37 50 37s10-37 50-37c45 0 52 42 52 60"
+							fill="#cbd5e1"
+						/>
+					</a>
+				</g>
+			</svg>
+		</span>
+		<!-- Website -->
+		<span class="col-span-1 text-center grid justify-items-center">
+			<a
+				class="transition ease-in-out hover:translate-y-1 hover:scale-110 duration-300"
+				href="https://www.closingtags.com/"
+				target="_blank"
+			>
 				<svg
 					class="w-[90px] -mt-[8px] transition ease-in-out duration-300 hover:stroke-[#8e3d20]"
 					viewBox="0 0 64 64"
@@ -68,15 +109,14 @@ Dylan Hildenbrand is a freelance full-stack web developer with a penchant for un
 					stroke-width="3"
 					stroke="#f97141"
 					fill="none"
-					>
-        <title>Dylan's Website</title>
-        <g id="SVGRepo_bgCarrier" stroke-width="0" /><g
+				>
+					<title>Dylan's Website</title>
+					<g id="SVGRepo_bgCarrier" stroke-width="0" /><g
 						id="SVGRepo_tracerCarrier"
 						stroke-linecap="round"
 						stroke-linejoin="round"
 					/><g id="SVGRepo_iconCarrier">
-
-            <path d="M39.93,55.72A24.86,24.86,0,1,1,56.86,32.15a37.24,37.24,0,0,1-.73,6" /><path
+						<path d="M39.93,55.72A24.86,24.86,0,1,1,56.86,32.15a37.24,37.24,0,0,1-.73,6" /><path
 							d="M37.86,51.1A47,47,0,0,1,32,56.7"
 						/><path d="M32,7A34.14,34.14,0,0,1,43.57,30a34.07,34.07,0,0,1,.09,4.85" /><path
 							d="M32,7A34.09,34.09,0,0,0,20.31,32.46c0,16.2,7.28,21,11.66,24.24"
@@ -93,19 +133,20 @@ Dylan Hildenbrand is a freelance full-stack web developer with a penchant for un
 						/><path
 							d="M53.57,57,58,52.56l-8-8,4.55-2.91a.38.38,0,0,0-.12-.7L39.14,37.37a.39.39,0,0,0-.46.46L42,53.41a.39.39,0,0,0,.71.13L45.57,49Z"
 						/>
-        </g>
-      </svg>
-          </a>
+					</g>
+				</svg>
+			</a>
 		</span>
 	</div>
 	<img
 		class="h-96 w-96 object-cover object-[center_top] rounded-full justify-self-center lg:order-2 order-1 py-2"
 		src={author}
-		alt="Dylan Hildenbrand posing handsomly."
+		alt="Dylan Hildenbrand poses with his arms crossed. He is a caucasion man wearing glasses, has tossled hair, and a short beard. He wears a black, long-sleeve shirt and has his arms crossed. He is also incredibly handsome. Like, so ridiculously handsome it's not even fair. How many other nerds do you know that look this good?"
 	/>
 </div>
+
 <style>
-  .svg_link:hover .fill_me {
-    fill: #8e3d20;
-  }
+	.svg_link:hover .fill_me {
+		fill: #8e3d20;
+	}
 </style>
