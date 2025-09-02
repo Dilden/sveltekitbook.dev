0c50d3f82613817232063ad141025d88a1c5e5eb
dylan <dylan.hildenbrand@gmail.com>
2025-09-02 15:11:39 -0500

    updated FAQ to talk about Svelte 5

--- src/lib/FAQ.svelte
+++ src/lib/FAQ.svelte
@@ -1,86 +1,166 @@
-<h2 class='text-2xl my-5'>FAQ</h2>
+<h2 class="text-2xl my-5">FAQ</h2>
 <details class="open:bg-[#f97141] bg-[#333232] duration-300 my-5">
-    <summary class="bg-inherit px-5 py-3 text-lg cursor-pointer">Why did you write this book?</summary>
-    <div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
-      <p>This book was written to be concise and simple resource for other developers looking to learn a next generation JavaScript framework. It was written to provide the explanations and examples I wish I had when learning <a href='https://kit.svelte.dev' class='text-amber-600 hover:underline hover:text-amber-800'>SvelteKit</a>. The examples aim to be practical yet provide real-world demonstrations utilizing the concepts powering SvelteKit. After finishing this book, readers will have a high level understanding of SvelteKit and be able to build and release their very own SvelteKit-based applications and web sites.</p>
-    </div>
+	<summary class="bg-inherit px-5 py-3 text-lg cursor-pointer"
+		>Does this book apply to Svelte 5 & Runes?</summary
+	>
+	<div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
+		<p>
+			Shortly after publication of this book, Svelte 5 was released. Since Svelte's new rune system
+			did not exist at the time of writing, it is not included in this text. Have no fear though;
+			the old syntax still works with Svelte 5 & core concepts should still apply! Because
+			technology moves fast, there may be some features that have changed within both Svelte and
+			SvelteKit. As always, refer to the <a
+				href="https://svelte.dev/docs/kit/introduction"
+				target="_blank">documentation</a
+			> for more details.
+		</p>
+	</div>
 </details>
 <details class="open:bg-[#f97141] bg-[#333232] duration-300 my-5">
-    <summary class="bg-inherit px-5 py-3 text-lg cursor-pointer">Who is this book for?</summary>
-    <div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
-      <p>
-        <b class='underline'>SvelteKit Up and Running</b> is intended for web developers looking to advance their skill set by learning
-        the next popular JS-based framework. Whether you’re new to web development and deciding which framework should be the first
-        you learn, or you want to set yourself apart from other job applicants by adding yet another popular
-        technology to your résumé, this book will provide you with all of the essential knowledge to begin your
-        journey to becoming a SvelteKit master!
-    </p>
-    </div>
+	<summary class="bg-inherit px-5 py-3 text-lg cursor-pointer">Why did you write this book?</summary
+	>
+	<div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
+		<p>
+			This book was written to be concise and simple resource for other developers looking to learn
+			a next generation JavaScript framework. It was written to provide the explanations and
+			examples I wish I had when learning <a
+				href="https://kit.svelte.dev"
+				class="text-amber-600 hover:underline hover:text-amber-800">SvelteKit</a
+			>. The examples aim to be practical yet provide real-world demonstrations utilizing the
+			concepts powering SvelteKit. After finishing this book, readers will have a high level
+			understanding of SvelteKit and be able to build and release their very own SvelteKit-based
+			applications and web sites.
+		</p>
+	</div>
 </details>
 <details class="open:bg-[#f97141] bg-[#333232] duration-300 my-5">
-    <summary class="bg-inherit px-5 py-3 text-lg cursor-pointer">Do I need anything before starting?</summary>
-    <div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
-      <p class='py-5'>
-        Having a working knowledge of <a href='https://developer.mozilla.org/en-US/docs/Web/HTML'>HTML</a>,
-        <a href='https://developer.mozilla.org/en-US/docs/Web/CSS'>CSS</a>, and 
-        <a href='https://developer.mozilla.org/en-US/docs/Web/javascript'>JS</a> are a must before 
-        beginning this book. Prior knowledge of <a href='https://svelte.dev'>Svelte</a> and how it works will be very beneficial.
-      </p>
-      <p class='py-5'>
-        It also helps to have a computer to follow along with the provided code. Following along with the examples will ensure you're getting the experience inteded by the author. You'll need a MacOS, Windows, or Linux based computer with access to console or terminal for executing commands. You'll also need <a href='https://github.com/nvm-sh/nvm'>Node.js v16 or higher</a> installed on your computer as well as a text editor. The code found in this book has been made <a href='https://github.com/PacktPublishing/SvelteKit-Up-and-Running'>freely available on GitHub</a>. Likewise, the code for this very website is <a href='https://github.com/Dilden/sveltekitbook.dev'>also available on GitHub</a>.
-      </p>
-    </div>
+	<summary class="bg-inherit px-5 py-3 text-lg cursor-pointer">Who is this book for?</summary>
+	<div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
+		<p>
+			<b class="underline">SvelteKit Up and Running</b> is intended for web developers looking to advance
+			their skill set by learning the next popular JS-based framework. Whether you’re new to web development
+			and deciding which framework should be the first you learn, or you want to set yourself apart from
+			other job applicants by adding yet another popular technology to your résumé, this book will provide
+			you with all of the essential knowledge to begin your journey to becoming a SvelteKit master!
+		</p>
+	</div>
 </details>
 <details class="open:bg-[#f97141] bg-[#333232] duration-300 my-5">
-    <summary class="bg-inherit px-5 py-3 text-lg cursor-pointer">What's in this book?</summary>
-    <div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
-      <p class='my-5'>
-        In the first part, you’ll be introduced to SvelteKit with a quick installation, followed up with the standard
-        Hello, World! example we’ve all come to love and appreciate. This will help familiarize you with SvelteKit's
-        project structure. It then goes on to lay some groundwork by exploring the various configuration options available to further
-        customize a SvelteKit application. From there, it covers how SvelteKit leverages existing standards
-        to deliver a small yet powerful framework.
-      </p>
-      <p class='my-5'>
-        In the second part, we learn why the developers of SvelteKit have made
-        routing central to the framework. It examines some previously introduced routing techniques
-        in more detail. This part then shows how SvelteKit moves data to Svelte components and accepts data from
-        them through HTML form elements. Finally, you’ll see some more advanced routing techniques that
-        promise to cover even the most obscure edge cases encountered in routing.
-      </p>
-      <p class='my-5'>
-        Part three aims to cover the additional requisite concepts of SvelteKit. It begins by showing you how to
-        produce a build for a production environment with resources showing how easily it can be done using
-        SvelteKit’s adapter system. It then covers how hooks can be used to manipulate data flowing in and out
-        of a SvelteKit application. From there, it addresses how to best import static assets by leveraging Vite.
-        Then, it explains the various modules available in SvelteKit that make the entire framework possible.
-        The section also covers how to ensure that a SvelteKit application can be made available to as many
-        users as possible while boosting search engine rankings. Finally, it wraps up with various resources
-        that will prove invaluable to any developer attempting to power their next project with SvelteKit.
-      </p>
-    </div>
+	<summary class="bg-inherit px-5 py-3 text-lg cursor-pointer"
+		>Do I need anything before starting?</summary
+	>
+	<div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
+		<p class="py-5">
+			Having a working knowledge of <a href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+				>HTML</a
+			>,
+			<a href="https://developer.mozilla.org/en-US/docs/Web/CSS">CSS</a>, and
+			<a href="https://developer.mozilla.org/en-US/docs/Web/javascript">JS</a> are a must before
+			beginning this book. Prior knowledge of <a href="https://svelte.dev">Svelte</a> and how it works
+			will be very beneficial.
+		</p>
+		<p class="py-5">
+			It also helps to have a computer to follow along with the provided code. Following along with
+			the examples will ensure you're getting the experience inteded by the author. You'll need a
+			MacOS, Windows, or Linux based computer with access to console or terminal for executing
+			commands. You'll also need <a href="https://github.com/nvm-sh/nvm">Node.js v16 or higher</a>
+			installed on your computer as well as a text editor. The code found in this book has been made
+			<a href="https://github.com/PacktPublishing/SvelteKit-Up-and-Running"
+				>freely available on GitHub</a
+			>. Likewise, the code for this very website is
+			<a href="https://github.com/Dilden/sveltekitbook.dev">also available on GitHub</a>.
+		</p>
+	</div>
 </details>
 <details class="open:bg-[#f97141] bg-[#333232] duration-300 my-5">
-    <summary class="bg-inherit px-5 py-3 text-lg cursor-pointer">Where can I get it?</summary>
-    <div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
-      <p>
-        SvelteKit Up and Running can be obtained in a DRM-free format directly from <a href='https://www.packtpub.com/product/sveltekit-up-and-running/9781804615485' class='text-amber-600 hover:underline hover:text-amber-800'>Packt Publishing</a>! 
-        It can also be purchased on <a href='https://packt.link/bVAyJ' class='text-amber-600 hover:underline hover:text-amber-800'>Amazon</a> but this author advises against purchasing there and instead recommends reading <a href='https://microcosmpublishing.com/catalog/zines/12043' target='_blank'>How to Resist Amazon and Why: The Fight for Local Economics, Data Privacy, Fair Labor, Independent Bookstores, and a People-Powered Future!</a> for various reasions why you should avoid using Amazon's platform.
-      </p>
-    </div>
+	<summary class="bg-inherit px-5 py-3 text-lg cursor-pointer">What's in this book?</summary>
+	<div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
+		<p class="my-5">
+			In the first part, you’ll be introduced to SvelteKit with a quick installation, followed up
+			with the standard Hello, World! example we’ve all come to love and appreciate. This will help
+			familiarize you with SvelteKit's project structure. It then goes on to lay some groundwork by
+			exploring the various configuration options available to further customize a SvelteKit
+			application. From there, it covers how SvelteKit leverages existing standards to deliver a
+			small yet powerful framework.
+		</p>
+		<p class="my-5">
+			In the second part, we learn why the developers of SvelteKit have made routing central to the
+			framework. It examines some previously introduced routing techniques in more detail. This part
+			then shows how SvelteKit moves data to Svelte components and accepts data from them through
+			HTML form elements. Finally, you’ll see some more advanced routing techniques that promise to
+			cover even the most obscure edge cases encountered in routing.
+		</p>
+		<p class="my-5">
+			Part three aims to cover the additional requisite concepts of SvelteKit. It begins by showing
+			you how to produce a build for a production environment with resources showing how easily it
+			can be done using SvelteKit’s adapter system. It then covers how hooks can be used to
+			manipulate data flowing in and out of a SvelteKit application. From there, it addresses how to
+			best import static assets by leveraging Vite. Then, it explains the various modules available
+			in SvelteKit that make the entire framework possible. The section also covers how to ensure
+			that a SvelteKit application can be made available to as many users as possible while boosting
+			search engine rankings. Finally, it wraps up with various resources that will prove invaluable
+			to any developer attempting to power their next project with SvelteKit.
+		</p>
+	</div>
 </details>
 <details class="open:bg-[#f97141] bg-[#333232] duration-300 my-5">
-    <summary class="bg-inherit px-5 py-3 text-lg cursor-pointer">Can you recommend any other SvelteKit resources?</summary>
-    <div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
-      <p class="my-5">The <a href="https://svelte.dev" target="_blank">Svelte</a> and <a href="https://kit.svelte.dev" target="_blank">SvelteKit</a> documentation are fantastic resources for getting started. There is also a wonderfully thorough <a href="https://learn.svelte.dev" target="_blank">tutorial</a> maintained by the official team that covers many subjects not discussed in this book.</p>
-      <p class="my-5">If you have finished <u><b>SvelteKit Up and Running</b></u> and are interested in taking your SvelteKit skills to the next level, check out <a href='https://www.amazon.com/Svelte-Test-Driven-Development-Playwright-Cucumber-js-ebook/dp/B0BZ8MFY14/ref=sr_1_5' target='_blank'>Svelte with Test-Driven Development</a> by Daniel Irvine. This incredible book dives into effective testing strategies for SvelteKit. Readers will learn how to incorporate <a href='https://vitest.dev/' target='_blank'>Vitest</a> to ensure each component behaves as expected. It also covers how to use <a href='https://playwright.dev/' target='_blank'>Playwright</a> to ensure users are getting the intended experience of the application. This resource will prove invaluable to those who wish to prove their code works correctly and is thoroughly tested.</p>
-    </div>
+	<summary class="bg-inherit px-5 py-3 text-lg cursor-pointer">Where can I get it?</summary>
+	<div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
+		<p>
+			SvelteKit Up and Running can be obtained in a DRM-free format directly from <a
+				href="https://www.packtpub.com/product/sveltekit-up-and-running/9781804615485"
+				class="text-amber-600 hover:underline hover:text-amber-800">Packt Publishing</a
+			>! It can also be purchased on
+			<a href="https://packt.link/bVAyJ" class="text-amber-600 hover:underline hover:text-amber-800"
+				>Amazon</a
+			>
+			but this author advises against purchasing there and instead recommends reading
+			<a href="https://microcosmpublishing.com/catalog/zines/12043" target="_blank"
+				>How to Resist Amazon and Why: The Fight for Local Economics, Data Privacy, Fair Labor,
+				Independent Bookstores, and a People-Powered Future!</a
+			> for various reasions why you should avoid using Amazon's platform.
+		</p>
+	</div>
 </details>
 <details class="open:bg-[#f97141] bg-[#333232] duration-300 my-5">
-    <summary class="bg-inherit px-5 py-3 text-lg cursor-pointer">I have more questions! What's the best way to reach you?</summary>
-    <div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
-      <p>
-        The author of this book (and website) can be reached via the contact form at <a href='https://closingtags.com/contact' target='_blank'>https://closingtags.com/contact</a>. He's also on the Fediverse at <a href='https://mastodon.social/@closingtags' target='_blank'>Mastodon</a>!
-      </p>
-    </div>
+	<summary class="bg-inherit px-5 py-3 text-lg cursor-pointer"
+		>Can you recommend any other SvelteKit resources?</summary
+	>
+	<div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
+		<p class="my-5">
+			The <a href="https://svelte.dev" target="_blank">Svelte</a> and
+			<a href="https://kit.svelte.dev" target="_blank">SvelteKit</a>
+			documentation are fantastic resources for getting started. There is also a wonderfully thorough
+			<a href="https://learn.svelte.dev" target="_blank">tutorial</a> maintained by the official team
+			that covers many subjects not discussed in this book.
+		</p>
+		<p class="my-5">
+			If you have finished <u><b>SvelteKit Up and Running</b></u> and are interested in taking your
+			SvelteKit skills to the next level, check out
+			<a
+				href="https://www.amazon.com/Svelte-Test-Driven-Development-Playwright-Cucumber-js-ebook/dp/B0BZ8MFY14/ref=sr_1_5"
+				target="_blank">Svelte with Test-Driven Development</a
+			>
+			by Daniel Irvine. This incredible book dives into effective testing strategies for SvelteKit. Readers
+			will learn how to incorporate <a href="https://vitest.dev/" target="_blank">Vitest</a>
+			to ensure each component behaves as expected. It also covers how to use
+			<a href="https://playwright.dev/" target="_blank">Playwright</a> to ensure users are getting the
+			intended experience of the application. This resource will prove invaluable to those who wish to
+			prove their code works correctly and is thoroughly tested.
+		</p>
+	</div>
+</details>
+<details class="open:bg-[#f97141] bg-[#333232] duration-300 my-5">
+	<summary class="bg-inherit px-5 py-3 text-lg cursor-pointer"
+		>I have more questions! What's the best way to reach you?</summary
+	>
+	<div class="bg-slate-300 px-5 py-3 border border-[#1a1a1a] text-[#1a1a1a]">
+		<p>
+			The author of this book (and website) can be reached via the contact form at <a
+				href="https://closingtags.com/contact"
+				target="_blank">https://closingtags.com/contact</a
+			>. He's also on the Fediverse at
+			<a href="https://mastodon.social/@closingtags" target="_blank">Mastodon</a>!
+		</p>
+	</div>
 </details>
